2592bf53a3ab8ff0001c79a198fc572e496ffa61
Tyler Pulse <tyler3321@gmail.com>
2017-11-09 17:55:43 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@
 def speak_to_grandma(phrase)
   if phrase
       puts "HUH! SPEAK UP, SONNY!"
-  elsif phrase.upper
+  elsif phrase.upcase
       puts "NO, NOT SINCE 1938!"
   else "I LOVE YOU GRANDMA!"
       puts "I LOVE YOU TOO PUMPKIN!"
