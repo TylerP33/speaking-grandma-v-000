fb1933c8fb8abef9c9f711564fe9d029e217825b
Tyler Pulse <tyler3321@gmail.com>
2017-11-09 18:21:46 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,16 +1,15 @@
 def speak_to_grandma(phrase)
-if phrase == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU PUMPKIN!"
-elsif phrase == "Hi Nana, how are you?"
-  puts "HUH?! SPEAK UP, SONNY!"
-elsif phrase == "Hi!"
-  puts "HUH?! SPEAK UP, SONNY!"
-elsif phrase == "WHAT DID YOU EAT TODAY?"
-  puts "NO, NOT SINCE 1938!"
-elsif phrase == "WHAT?"
-  puts "NO, NOT SINCE 1938!"
   if phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU PUMPKIN!"
-
-end
+      puts "I LOVE YOU PUMPKIN!"
+  elsif phrase == "Hi Nana, how are you?"
+      puts "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "Hi!"
+      puts "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "WHAT DID YOU EAT TODAY?"
+      puts "NO, NOT SINCE 1938!"
+  elsif phrase == "WHAT?"
+      puts "NO, NOT SINCE 1938!"
+    if phrase == "I LOVE YOU GRANDMA!"
+      puts "I LOVE YOU PUMPKIN!"
+    end
 end
